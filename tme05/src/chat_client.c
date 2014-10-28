7fc083a02307574f5326b3950a48314f95ac3b24
Boris Berger <berger.boris@orange.fr>
2014-10-28 01:46:38 +0100

    exo3 -> chat_server.c chat_client.c

---
+++ tme05/src/chat_client.c
@@ -0,0 +1,167 @@
+#define _XOPEN_SOURCE 700
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#define ST_SIZE 32
+#define BUF_SIZE 1024
+
+struct request {
+    long type;
+    char content[BUF_SIZE];
+};
+
+/* Global variables */
+struct request *msg_in;
+sem_t *sem_read,*sem_write;
+char last[BUF_SIZE];
+
+/* Display thread */
+void* display_thread(){
+    for(;;)
+    {
+        sem_wait(sem_read);
+        if(strcmp(last,msg_in->content)!=0)
+            if ((fprintf(stdout, msg_in->content)) < 0 )
+                perror("print");
+        sem_post(sem_write);
+    }
+}
+
+
+int main(int argc, char ** argv){
+    struct request *msg;
+
+    /* Server variables */
+    sem_t *sem_msgread, *sem_msgwrite;
+    int shm_server;
+    char st_msgread[ST_SIZE], st_msgwrite[ST_SIZE], st_shm_server[ST_SIZE];
+
+    /* Client variables */
+    int shm_client;
+    char buf[BUF_SIZE],st_read[ST_SIZE],st_write[ST_SIZE],st_shm_client[ST_SIZE];
+
+    if(argc != 3){
+        printf("usage : %s server_id client_id",argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    /* Create connection strings  */
+    sprintf(st_msgread,"/%s_sem:0",argv[1]);
+    sprintf(st_msgwrite,"/%s_sem:1",argv[1]);
+    sprintf(st_shm_server,"/%s_shm:0",argv[1]);
+
+    sprintf(st_read,"/%s_sem:0",argv[2]);
+    sprintf(st_write,"/%s_sem:1",argv[2]);
+    sprintf(st_shm_client,"/%s_shm:0",argv[2]);
+
+    /* Access server shareds ressources : 2 SEM & 1 SHM */
+    sem_msgread = sem_open(st_msgread,O_RDWR);
+    if(sem_msgread == SEM_FAILED){
+        fprintf(stderr,"sem_open %s",st_msgread);
+        return EXIT_FAILURE;
+    }
+    sem_msgwrite = sem_open(st_msgwrite,O_RDWR);
+    if(sem_msgwrite == SEM_FAILED){
+        fprintf(stderr,"sem_open %s",st_msgwrite);
+        return EXIT_FAILURE;
+    }
+    shm_server = shm_open(st_shm_server,O_RDWR,0600);
+    if(shm_server == -1){
+        fprintf(stderr,"shm_open %s",st_shm_server);
+        return EXIT_FAILURE;
+    }
+
+    /* Map SHM*/
+    msg = mmap(NULL,sizeof(struct request),
+            PROT_READ|PROT_WRITE,MAP_SHARED,shm_server,0);
+    if(msg == MAP_FAILED){
+        perror("mmap");
+        return EXIT_FAILURE;
+    }
+
+    /* Create client shareds ressources : 2 SEM & 1 SHM */
+    sem_read = sem_open(st_read,O_RDWR|O_CREAT,0666,0);
+    if(sem_read == SEM_FAILED){
+        fprintf(stderr,"sem_open %s",st_read);
+        return EXIT_FAILURE;
+    }
+    sem_write = sem_open(st_write,O_RDWR|O_CREAT,0666,1);
+    if(sem_write == SEM_FAILED){
+        fprintf(stderr,"sem_open %s",st_write);
+        return EXIT_FAILURE;
+    }
+    shm_client = shm_open(st_shm_client,O_RDWR|O_CREAT,0600);
+    if(shm_client == -1){
+        fprintf(stderr,"shm_open %s",st_shm_client);
+        return EXIT_FAILURE;
+    }
+
+    /* Truncate and map SHM*/
+    if(ftruncate(shm_client,sizeof(struct request)) == -1){
+        perror("ftruncate");
+        return EXIT_FAILURE;
+    }
+
+    msg_in = mmap(NULL,sizeof(struct request),
+            PROT_READ|PROT_WRITE,MAP_SHARED,shm_server,0);
+    if(msg_in == MAP_FAILED){
+        perror("mmap");
+        return EXIT_FAILURE;
+    }
+
+    /* Connect */
+    printf("Connecting to %s...\n",argv[1]);
+    sem_wait(sem_msgwrite);
+    msg->type = 0;
+    sprintf(msg->content,"%s",argv[2]);
+    sem_post(sem_msgread);
+    printf("Connected.\n");
+
+    /* Main Loop */
+    int i;
+    int ir = 1;
+
+    pthread_t t;
+    pthread_create(&t, NULL, display_thread, NULL);
+
+    while (ir > 0) 
+    {
+        for (i = 0; i < BUF_SIZE ; i++)
+            buf[i] = '\0';
+        if ((ir = read(STDIN_FILENO, buf, BUF_SIZE)) == -1)
+            perror("read");
+        if(strcmp(buf,"exit\n") == 0)
+            break;
+        sem_wait(sem_msgwrite);
+        msg->type = 1;
+        sprintf(msg->content,buf);
+        sem_post(sem_msgread);
+        sprintf(last,buf);
+    }
+
+    /* Disconnect */
+    printf("Disconnecting from %s...\n",argv[1]);
+    sem_wait(sem_msgwrite);
+    msg->type = 2;
+    sprintf(msg->content,"%s",argv[2]);
+    sem_post(sem_msgread);
+    printf("Disconnected.\n");
+ 
+
+    /* Closing and unlinking shared ressources*/
+    sem_close(sem_msgread);
+    sem_close(sem_msgwrite);
+    munmap(msg,sizeof(struct request));
+
+    return EXIT_SUCCESS;
+}
---
+++ tme05/src/chat_server.c
@@ -0,0 +1,172 @@
+#define _XOPEN_SOURCE 700
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#define MAX_CLIENTS 256
+#define ST_SIZE 32
+#define BUF_SIZE 1024
+
+struct request {
+    long type;
+    char content[BUF_SIZE];
+};
+
+
+int main(int argc, char ** argv){
+    struct request *msg, *msg_out;
+
+    /* Server variables */
+    sem_t *sem_msgread, *sem_msgwrite;
+    int shm;
+    char st_msgread[ST_SIZE], st_msgwrite[ST_SIZE], st_shm[ST_SIZE];
+
+    /* Clients variables*/
+    char *clients[MAX_CLIENTS];
+    int shm_client[MAX_CLIENTS];
+    sem_t *sem_write_client[MAX_CLIENTS], *sem_read_client[MAX_CLIENTS];
+    char st_read[ST_SIZE], st_write[ST_SIZE], st_shm_client[ST_SIZE];
+
+    if(argc != 2){
+        printf("usage : %s server_id",argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    /* Create connection strings  */
+    sprintf(st_msgread,"/%s_sem:0",argv[1]);
+    sprintf(st_msgwrite,"/%s_sem:1",argv[1]);
+    sprintf(st_shm,"/%s_shm:0",argv[1]);
+
+    /* Create shareds ressources : 2 SEM & 1 SHM */
+    sem_msgread = sem_open(st_msgread,O_RDWR|O_CREAT,0666,0);
+    if(sem_msgread == SEM_FAILED){
+        fprintf(stderr,"sem_open %s",st_msgread);
+        return EXIT_FAILURE;
+    }
+    sem_msgwrite = sem_open(st_msgwrite,O_RDWR|O_CREAT,0666,1);
+    if(sem_msgwrite == SEM_FAILED){
+        fprintf(stderr,"sem_open %s",st_msgwrite);
+        return EXIT_FAILURE;
+    }
+    shm = shm_open(st_shm,O_RDWR|O_CREAT,0666);
+    if(shm == -1){
+        fprintf(stderr,"shm_open %s",st_shm);
+        return EXIT_FAILURE;
+    }
+
+    /* Truncate and map SHM*/
+    if(ftruncate(shm,sizeof(struct request)) == -1){
+        perror("ftruncate");
+        return EXIT_FAILURE;
+    }
+
+    msg = mmap(NULL,sizeof(struct request),
+            PROT_READ|PROT_WRITE,MAP_SHARED,shm,0);
+    if(msg == MAP_FAILED){
+        perror("mmap");
+        return EXIT_FAILURE;
+    }
+
+    /* Main loop*/
+    int i;
+    for(i=0;i<MAX_CLIENTS;i++){
+        clients[i] = NULL;
+    }
+
+    for(;;){
+        sem_wait(sem_msgread);
+        /* Connection */
+        if(msg->type == 0){
+            for(i = 0;i<MAX_CLIENTS;i++){
+                if(clients[i] == NULL)
+                {
+                    clients[i] = strdup(msg->content);
+
+                    /* Create connection strings for client */
+                    sprintf(st_read,"/%s_sem:0",clients[i]);
+                    sprintf(st_write,"/%s_sem:1",clients[i]);
+                    sprintf(st_shm_client,"/%s_shm:0",clients[i]);
+
+                    /* Access client shared ressources : 2 SEM & 1 SHM  */
+                    sem_read_client[i] = sem_open(st_read,O_RDWR);
+                    if(sem_read_client[i] == SEM_FAILED){
+                        fprintf(stderr,"sem_open %s",st_read);
+                        return EXIT_FAILURE;
+                    }
+                    sem_write_client[i] = sem_open(st_write,O_RDWR);
+                    if(sem_write_client[i] == SEM_FAILED){
+                        fprintf(stderr,"sem_open %s",st_write);
+                        return EXIT_FAILURE;
+                    }
+                    shm_client[i] = shm_open(st_shm_client,O_RDWR,0600);
+                    if(shm_client[i] == -1){
+                        fprintf(stderr,"shm_open %s",st_shm_client);
+                        return EXIT_FAILURE;
+                    }
+
+                    printf("Connected : %s\n",clients[i]);
+                    printf("  sem_read : %s\n",st_read);
+                    printf("  sem_write : %s\n",st_write);
+                    printf("  shm : %s\n",st_shm_client);
+                    break;
+                }
+            }
+        }
+        /* Disconnection */
+        else if(msg->type == 2){
+            for(i=0;i<MAX_CLIENTS;i++){
+                if(clients[i] != NULL){
+                    if (strcmp(clients[i],msg->content)==0)
+                    {
+                        printf("Disconnecting %s...\n",clients[i]);
+                        free(clients[i]);
+                        sem_close(sem_write_client[i]);
+                        sem_close(sem_read_client[i]);
+                        clients[i] = NULL;
+                        printf("Disconnected.\n");
+                        break;
+                    }
+                }
+            }
+
+        }
+        /* Message */
+        else if(msg->type == 1){
+            for(i = 0;i<MAX_CLIENTS;i++){
+                if(clients[i] != NULL)
+                {
+                    msg_out = mmap(NULL,sizeof(struct request),
+                            PROT_READ|PROT_WRITE,MAP_SHARED,shm_client[i],0);
+                    if(msg_out == MAP_FAILED){
+                        perror("mmap");
+                        return EXIT_FAILURE;
+                    }
+                    sem_wait(sem_write_client[i]);
+                    strncpy(msg_out->content,msg->content,BUF_SIZE);
+                    sem_post(sem_read_client[i]);
+                    munmap(msg_out, sizeof(struct request));
+                }
+            }
+
+        }
+        sem_post(sem_msgwrite);
+    }
+
+    /* Closing and unlinking shared ressources*/
+    sem_close(sem_msgread);
+    sem_close(sem_msgwrite);
+    munmap(msg,sizeof(struct request));
+
+    sem_unlink(st_msgread);
+    sem_unlink(st_msgwrite);
+    shm_unlink(st_shm);
+
+    return EXIT_SUCCESS;
+}
