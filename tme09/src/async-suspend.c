60f5b40a874476aee098ad06dc1e32261acb7d76
Leo Testard <leo.testard@gmail.com>
2014-12-16 18:18:40 +0100

    Add ex1b.

---
+++ tme09/src/async-suspend.c
@@ -0,0 +1,50 @@
+#define _POSIX_SOURCE 1
+
+#include <aio.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void on_sig(int sig) { (void) sig; }
+
+int main(int argc, char **argv)
+{
+    if(argc != 3 || !strcmp(argv[1], "-h")) {
+        fprintf(stderr, "usage: %s FILE TEXT...\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char* file = argv[1];
+    char* text = argv[2];
+    int len = strlen(text);
+
+    int fd = open(file, O_WRONLY | O_CREAT, 0600);
+    struct aiocb aio;
+    aio.aio_fildes = fd;
+    aio.aio_buf = text;
+    aio.aio_nbytes = len;
+    aio.aio_offset = 0;
+    aio.aio_reqprio = 0;
+    aio.aio_sigevent.sigev_notify = SIGEV_NONE;
+    aio_write(&aio);
+
+    fd = open(file, O_RDONLY, 0600);
+
+    struct aiocb const* const aios = &aio;
+    aio_suspend(&aios, 1, NULL);
+
+    char* buf = malloc(len);
+    aio.aio_fildes = fd;
+    aio.aio_buf = buf;
+    aio.aio_nbytes = len;
+    aio_read(&aio);
+    aio_suspend(&aios, 1, NULL);
+
+    printf("read: %s\n", buf);
+
+    return 0;
+}
