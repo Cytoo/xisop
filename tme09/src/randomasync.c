bc8c40ae055a28e655158380711b27f16e91452c
Leo Testard <leo.testard@gmail.com>
2014-12-16 18:56:52 +0100

    Add ex3.

---
+++ tme09/src/randomasync.c
@@ -0,0 +1,88 @@
+#define _XOPEN_SOURCE 700
+
+#include <aio.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv)
+{
+    int i, nb_proc, total;
+
+    if(argc != 2 || !strcmp(argv[1], "-h"))
+    {
+        fprintf(stderr, "usage: %s NB_THREADS\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    nb_proc = atoi(argv[1]);
+    total = 0;
+
+    char* path = "random.ipc";
+    int fd;
+
+    if((fd = open(path, O_RDONLY | O_CREAT | O_TRUNC, 0644)) == -1)
+    {
+        perror("unable to open IPC file");
+        return EXIT_FAILURE;
+    }
+
+    for(i = 0; i < nb_proc; ++i)
+    {
+        if(fork() == 0)
+        {
+            srand(time(NULL) ^ (getpid() << 16));
+            int rand_val = (int) (10 * (float) rand() / RAND_MAX);
+
+            if((fd = open(path, O_RDWR)) == -1)
+            {
+                perror("[%d] unable to reopen IPC file");
+                return EXIT_FAILURE;
+            }
+            
+            struct aiocb aio;
+            aio.aio_fildes = fd;
+            aio.aio_buf = (void*) &rand_val;
+            aio.aio_nbytes = sizeof(int);
+            aio.aio_offset = i * sizeof(int);
+            aio.aio_reqprio = 0;
+            aio.aio_sigevent.sigev_notify = SIGEV_NONE;
+            aio_write(&aio);
+
+            struct aiocb const* const aios = &aio;
+            aio_suspend(&aios, 1, NULL);
+
+            close(fd);
+            return EXIT_SUCCESS;
+        }
+    }
+
+    for(i = 0; i < nb_proc; ++i)
+        wait(NULL);
+
+    int* buf = malloc(sizeof(int*) * nb_proc);
+    struct aiocb aio;
+    aio.aio_fildes = fd;
+    aio.aio_buf = (void*) buf;
+    aio.aio_nbytes = nb_proc * sizeof(int);
+    aio.aio_offset = 0;
+    aio.aio_reqprio = 0;
+    aio.aio_sigevent.sigev_notify = SIGEV_NONE;
+    aio_read(&aio);
+
+    struct aiocb const* const aios = &aio;
+    aio_suspend(&aios, 1, NULL);
+
+    for(i = 0; i < nb_proc; ++i) {
+        total += buf[i];
+    }
+
+    printf("total = %d\n", total);
+    close(fd);
+
+    return EXIT_SUCCESS;
+}
